1c6da6ffdf50eadace83ef5725bbb5d7b775d5bd
Salmaan <salmaan.arif.ali@gmail.com>
2021-02-16 01:23:12 -0500

    commit tree 392a2d5f7e42bcdb78c2a16f917823bae2fedf4e

--- lib/student.rb
+++ lib/student.rb
@@ -2,5 +2,53 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
-  
+
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        );
+      SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade) VALUES (?,?);
+      SQL
+    
+    DB[:conn].execute(sql, self.name, self.grade)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+
+  def self.create(attr_hash)
+    student = self.new(nil,nil)
+
+    attr_hash.each do |key, value|
+      student.send("#{key}=", value)
+    end
+
+    student.save
+    student
+  end
 end
